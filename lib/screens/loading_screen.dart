4fecad303f3024983561194db3dcdc6c18446052
Allabergen <allromis@gmail.com>
2019-07-15 03:20:58 -0500

    exception handling, work with json, http package, sample weather data fetch

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,7 +16,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            // getLocation();
+            getData();
           },
           child: Text('Get Location'),
         ),
@@ -21,9 +25,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      String name = decodedData['name'];
+      double temp = decodedData['main']['temp'];
+      int id = decodedData['weather'][0]['id'];
+
+      print(name);
+      print(temp);
+      print(id);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 }
